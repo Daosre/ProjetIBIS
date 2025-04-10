3ce0884bef1c5e89043b8ff0361af7cdc06f2b74
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-10 18:01:24 +0200

    get meal by country or category

--- pages/home/script/service/getAllCategorie.js
+++ pages/home/script/service/getAllCategorie.js
@@ -1,3 +1,5 @@
+const category = document.querySelector("#category");
+const categoryDefaultValue = country.value;
 class CategorieList {
   constructor() {
     this.categoriesInput = document.querySelector("#categorie");
@@ -15,6 +17,22 @@ class CategorieList {
       }
     );
   };
+  getMealsByCategory = async () => {
+    await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${this.categoriesInput.value}`
+    ).then(async (response) => {
+      let data = await response.json();
+      data.meals.map((meal) => {
+        const happyMeal = new Meal(meal.idMeal, meal.strMeal, meal.strMealThumb);
+        happyMeal.getMoreDetail();
+      });
+    });
+  };
 }
 const categorieMeal = new CategorieList();
 categorieMeal.getCategorie();
+category.addEventListener("change", () => {
+  if (country.value != categoryDefaultValue) {
+    countryMeal.getMealsByCategory();
+  }
+});
--- pages/home/script/service/getIngredient.js
+++ pages/home/script/service/getIngredient.js
@@ -4,15 +4,11 @@ class IngredientSearch {
   constructor() {}
   getMealsbyIngredient = async () => {
     let ingredientInput = document.querySelector("#ingredient");
-
     await fetch(
       `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredientInput.value}`
     ).then(async (response) => {
       console.log(response);
-
       let data = await response.json();
-      // console.log(data);
-
       data.meals.map((meal) => {
         const happyMeal = new Meal(meal.idMeal, meal.strMeal, meal.strMealThumb);
         happyMeal.getMoreDetail();
